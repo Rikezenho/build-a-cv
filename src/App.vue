58849fd3f3187944a28d43a38e319ab0836198bc
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-06-26 02:45:49 -0300

    Makes the content builder more generic

--- src/App.vue
+++ src/App.vue
@@ -9,63 +9,66 @@
     <div class="conteudo">
       <div class="content">
         <div
-          v-if="!!data.description && (!!data.languages.length)"
+          v-if="!!data.description"
           class="bio-wrapper">
           <h2>Sobre</h2>
           <div class="sobre">
             <span v-html="data.description" />
           </div>
-          <div class="bio">
-            <div
-              v-if="!!data.languages.length"
-              class="item">
-              <strong>Idiomas:</strong><br>
-              {{ data.languages.join(', ') }}
-            </div>
+          <div class="bio" v-if="!!data.languages.length">
+            <strong>Idiomas:</strong><br>
+            {{ data.languages.join(', ') }}
           </div>
         </div>
 
         <div
-          v-if="!!data.history.length"
-          class="history-wrapper has-hr">
-          <h2>Experiência profissional</h2>
-          <ul class="ul-history">
-            <li
-              v-for="(history, index) in data.history"
-              :key="`history-${index}`"
-              class="item">
-              <job :data="history" />
-            </li>
-          </ul>
-        </div>
+          v-for="(content, key) in data.additionalContent"
+          :key="`additional-${key}`"
+          class="has-hr"
+        >
+          <h2>{{ content.title }}</h2>
+          <template
+            v-for="(subContent, subContentIndex) in content.content"
+          >
+            <ul
+              class="ul-history"
+              v-if="subContent.type === 'history'"
+              :key="`history-${subContentIndex}`"
+            >
+              <li
+                v-for="(history, index) in subContent.data"
+                :key="`history-${index}`"
+                class="item">
+                <job :data="history" />
+              </li>
+            </ul>
 
-        <div
-          v-if="!!data.complementary.length"
-          class="complementary-wrapper has-hr avoid-page-break">
-          <h2>Cursos complementares</h2>
-          <ul class="ul-complementary-courses">
-            <li
-              v-for="(course, index) in data.complementary"
-              :key="`course-${index}`"
-              class="item">
-              <course :data="course" />
-            </li>
-          </ul>
-        </div>
+            <ul
+              class="ul-courses"
+              v-if="subContent.type === 'courses'"
+              :key="`courses-${subContentIndex}`"
+            >
+              <li
+                v-for="(course, index) in subContent.data"
+                :key="`course-${index}`"
+                class="item">
+                <course :data="course" />
+              </li>
+            </ul>
 
-        <div
-          v-for="(nodeList, index) in data.nodeLists"
-          :key="nodeList.title"
-          :class="`${index}-wrapper has-hr avoid-page-break`">
-          <h2>{{ nodeList.title }}</h2>
-          <ul class="ul-nodelists">
-            <li
-              v-for="(node, key) in nodeList.items"
-              :key="`node-${key}`"
-              class="item">
-              <div class="box">{{ node }}</div>
-            </li>
-          </ul>
+            <ul
+              class="ul-nodelists"
+              v-if="subContent.type === 'nodelist'"
+              :key="`nodelist-${subContentIndex}`"
+            >
+              <li
+                v-for="(node, key) in subContent.data"
+                :key="`node-${key}`"
+                class="item">
+                <div class="box">{{ node }}</div>
+              </li>
+            </ul>
+          </template>
         </div>
 
       </div>
